9eba6141c6080c71b1be6215a2723c13851b1844
BartBizz <gabrielbart150@gmail.com>
2022-11-03 10:43:12 +0000

    03/11

---
+++ contato.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Dúvidas</title>
+        <style>
+            .cor{
+                background-color = black;
+            }
+        </style>
+    </head>
+    <body>
+        <center>
+        <h1>Dúvidas</h1>
+        <form action="enviar_email.php" method="post">
+            <label for="nome">Digite seu nome</label>
+            <input type="text" required name="nome"/><br><br>
+            <label for="email">Digite seu e-mail</label>
+            <input type="email" required name="email"/><br><br>
+            <label for="mensagem">Digite sua mensagem</label>
+            <textarea name="mensagem"></textarea><br><br>
+            <button type="submit">Enviar</button>
+        </center>
+        </form>
+    </body>
+</html>
---
+++ enviar_email.php
@@ -0,0 +1,46 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$mensagem = $_POST['mensagem'];
+
+$status = null;
+
+try {
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'bizzottogabriel150@gmail.com';
+    $mail->Password = 'comy jtba mokv armc';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+
+    $mail->setFrom($email, $nome);
+    $mail->addAddress('gabrielbart150@gmail.com','Gabriel');
+
+    $mail->isHTML(true);
+    $mail->Subject = 'formulário de contato - ' . $email;
+    $mail->Body = $mensagem;
+
+    $mail->send();
+    echo 'Mensagem enviada';
+}catch (Exception $e) {
+    echo "Mensagem não pode ser enviada. Mailer Error: {$mail->ErrorInfo}";
+}
+
+?>
+
+<h1>NOME</h1>
+<?=$nome?>
+<h1>EMAIL</h1>
+<?=$email?>
+<h1>MENSAGEM</h1>
+<?=$mensagem?>
